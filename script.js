750ac9baa73836c60eea0f9eee582e2bb6293af4
Shaundai Person <shaundaiperson@gmail.com>
2019-08-30 23:07:42 -0400

    Added ability to use notify me button

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-const drawer = document.getElementById('drawer');
+let drawer = document.getElementById('drawer');
+let notifyButton = document.getElementById('firstbutton')
 let isOpen = false;
-
-let openDrawer = () => {
-if (isOpen === false){
-
-};
+/*let closedProperties = function(){
+    if (isOpen === false){
+        
+    };
 }
+
+let openProperties = function(){
+    if (isOpen === true){
+        
+    };
+}*/
+
+let openDrawer = function() {
+    if (isOpen === false){
+    drawer.style.animation = 'slidein-left 2s'
+    }
+    isOpen = true;
+};
+
+let closeDrawer = function() {
+    if (isOpen === true){
+    drawer.style.animation = 'slideout-right 2s'
+    }
+    isOpen = false;
+};
+
+notifyButton.addEventListener('click', openDrawer);
