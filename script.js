85f4d5ff6b52eb6f972bf52cec1f600b42419838
Shaundai Person <shaundaiperson@gmail.com>
2019-08-31 19:45:28 -0400

    fully done!

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 let drawer = document.getElementById('drawer');
-let notifyButton = document.getElementById('firstbutton')
+let notifyButton = document.getElementById('firstbutton');
+let page = document.getElementById('page');
+let goAway = document.getElementById('goaway');
+let progressBar = document.getElementById('progress');
 let isOpen = false;
-/*let closedProperties = function(){
-    if (isOpen === false){
-        
-    };
-}
-
-let openProperties = function(){
-    if (isOpen === true){
-        
-    };
-}*/
 
 let openDrawer = function() {
     if (isOpen === false){
-    drawer.style.animation = 'slidein-left 2s'
-    }
+    drawer.style.animation = 'slidein-left 1s'
+    };
+    drawer.focus();
     isOpen = true;
+    drawer.style.transform = 'translateX(0)';
+    page.style.display = "block";
 };
 
 let closeDrawer = function() {
     if (isOpen === true){
-    drawer.style.animation = 'slideout-right 2s'
-    }
+    drawer.style.animation = 'slideout-right 1.5s'
+    };
     isOpen = false;
+    drawer.style.transform = 'translateX(130%)';
+    page.style.display = "none";
 };
 
 notifyButton.addEventListener('click', openDrawer);
+goAway.addEventListener('click', closeDrawer);
+
